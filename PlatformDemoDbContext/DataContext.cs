fbc4914c5695703b0a77e30b7aebf90190e6abbf
Andrew Coros <andrew_d213@yahoo.com>
2024-10-05 08:20:55 +0800

    added .net 8 class library for dbContext

---
+++ PlatformDemoDbContext/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace PlatformDemoDbContext
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+    }
+}
