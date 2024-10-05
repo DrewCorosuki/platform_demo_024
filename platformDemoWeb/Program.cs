5ce6f890667cc9cfdffb92660c17dc74e8cc770f
Andrew Coros <andrew_d213@yahoo.com>
2024-10-05 08:27:47 +0800

    add project references and nuget packages such as entity framework core and entity framework core sql server

--- platformDemoWeb/Program.cs
+++ platformDemoWeb/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformDemoDbContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("PlatformDemoContext")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
